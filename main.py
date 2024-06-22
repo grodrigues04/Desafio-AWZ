c1173162f954fb7788bfb43e52147e0299441efc
Gustavo Bernardo <gurodrigues.b@gmail.com>
2024-06-22 20:12:00 -0300

    fix: removed dict of directory

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import pandas as pd
 
-from sales import sales_employees_data, sales_directory_data
+from sales import sales_employees_data
 from payments import incorrect_payments
 
 def generateEmployeeCSV():
-    sales_employees_data["manager"] = sales_directory_data["manager"]
-    sales_employees_data["marketing_team"] = sales_directory_data["marketing_team"]
     df = pd.DataFrame(sales_employees_data)
     df_transposed = df.transpose() #Coluna vira linha, e linha vira coluna
     df_transposed = df_transposed.rename_axis("Nome do Vendedor").reset_index() # Colocando nome a coluna que ficou em branco ao ser transpostada
