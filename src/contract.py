67121ead9f9508201e95d90c458798d2d9a0e3cd
Gustavo Bernardo <gurodrigues.b@gmail.com>
2024-06-23 17:34:28 -0300

    chore: remove unexpected prints

--- src/contract.py
+++ src/contract.py
@@ -7,7 +7,6 @@ member_actions = {}
 def coletingMemberShares():
     for line in cpf:
         mainLine = line[0]
-        print(mainLine)
         comma_of_name = mainLine.find(",") #Virgula que vem logo apos o nome
         dot_of_name = mainLine.find(".") #Ponto depois dos numeros das listas
         name = mainLine[dot_of_name+2:comma_of_name:1]
