2f51256cc2553e0efd430f1433d31dddd5aa537c
Gustavo Bernardo <gurodrigues.b@gmail.com>
2024-06-23 18:44:09 -0300

    feat: add excess treatment to dataFrame

--- src/payments.py
+++ src/payments.py
@@ -4,22 +4,26 @@ from src.utils.converterToInt import converterToInt
 
 def get_incorrect_payments():
     try:
-        path_payments = "./data/spreadsheets/Venda - Pagamentos.csv"
+        path_payments = "./data/spreadsheets/Vendas - Pagamentos.csv"
         df_payments = pd.read_csv(path_payments)
     except FileNotFoundError:
         print('O arquivo de pagamentos näo foi encontrado.')
         return
 
     incorrect_payments = {}
-
-    for index, row in df_payments.iterrows():
-        seller_name = row["Nome do Vendedor"]
-        preliminary_commission = row["Comissão"]
-        preliminary_commission_int = converterToInt(preliminary_commission)
-        oficial_comission = get_sales_employees_data()[seller_name]["Comissão a Receber"]
-        if oficial_comission != preliminary_commission_int:
-            incorrect_payments[seller_name] = {
-                "Valor recebido": f"{preliminary_commission}",
-                "Valor Correto": f"R${oficial_comission}",
-            }
+    try:
+        for index, row in df_payments.iterrows():
+            seller_name = row["Nome do Vendedor"]
+            preliminary_commission = row["Comissão"]
+            preliminary_commission_int = converterToInt(preliminary_commission)
+            oficial_comission = get_sales_employees_data()[seller_name]["Comissão a Receber"]
+            if oficial_comission != preliminary_commission_int:
+                incorrect_payments[seller_name] = {
+                    "Valor recebido": f"{preliminary_commission}",
+                    "Valor Correto": f"R${oficial_comission}",
+                }
+    except KeyError as e:
+        print(f"Coluna nâo encontrada: {e}")
+        print("Verifique as colunas da planilha de pagamentos")
+        return 
     return incorrect_payments
