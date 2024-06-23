520828e8d33f5409322969a00d80e3d2b224f2b2
Gustavo Bernardo <gurodrigues.b@gmail.com>
2024-06-23 17:31:14 -0300

    fix: changing the search value

--- src/contract.py
+++ src/contract.py
@@ -4,14 +4,18 @@ cpf = sepateByKeyWord("CPF")
 
 member_actions = {}
 
-for line in cpf:
-    mainLine = line[0]
-    comma_of_name = mainLine.find(",") #Virgula que vem logo apos o nome
-    dot_of_name = mainLine.find(".") #Ponto depois dos numeros das listas
-    name = mainLine[dot_of_name+2:comma_of_name:1]
-    
-    shares = r'(\d+)\s+cotas'
-    matches = re.findall(shares, mainLine)
-    shares = [int(match) for match in matches]
-    member_actions[name] = shares
+def coletingMemberShares():
+    for line in cpf:
+        mainLine = line[0]
+        print(mainLine)
+        comma_of_name = mainLine.find(",") #Virgula que vem logo apos o nome
+        dot_of_name = mainLine.find(".") #Ponto depois dos numeros das listas
+        name = mainLine[dot_of_name+2:comma_of_name:1]
+        shares = r'(\d+)\s+cota'
+        matches_share = re.findall(shares, mainLine)
+        member_actions[name] = {
+            "cotas" : matches_share[0]
+        }
+    return member_actions
 
+print(coletingMemberShares())
